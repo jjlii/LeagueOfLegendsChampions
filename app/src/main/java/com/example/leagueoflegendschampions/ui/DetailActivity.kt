a6561a9f9a221e0562f0cd27d659485e8dc11ee4
Junjie Li <jjtommyliii@gmail.com>
2020-11-15 17:24:54 +0100

    WIP Detail Activity UI

--- app/src/main/java/com/example/leagueoflegendschampions/ui/DetailActivity.kt
+++ app/src/main/java/com/example/leagueoflegendschampions/ui/DetailActivity.kt
@@ -2,11 +2,65 @@ package com.example.leagueoflegendschampions.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import com.example.leagueoflegendschampions.BaseURL
 import com.example.leagueoflegendschampions.R
+import com.example.leagueoflegendschampions.databinding.ActivityDetailBinding
+import com.example.leagueoflegendschampions.loadUrl
+import com.example.leagueoflegendschampions.module.Champion
 
 class DetailActivity : AppCompatActivity() {
+
+    companion object{
+        const val CHAMPION = "DetailActivity:champion"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        intent.getParcelableExtra<Champion>(CHAMPION)?.run {
+            with(binding){
+                championDetailToolbar.title = name
+                championDetailImage.loadUrl(BaseURL.IMAGE_BASE_URL +image.full, 400)
+                championDetailSummary.text = blurb
+
+                championDetailInfo.text = buildSpannedString {
+
+                    bold { append("Title") }
+                    appendLine(title)
+
+                    bold { append("Partype") }
+                    append(partype)
+
+                    bold { append("Info") }
+                    appendLine("Attack: " + info.attack)
+                    appendLine("Defense: " + info.defense)
+                    appendLine("Magic: "+ info.magic)
+                    appendLine("Difficulty: "+info.difficulty)
+
+                    bold { append("Tags") }
+                    tags.forEach {
+                        appendLine(it)
+                    }
+
+                    bold { append("Stats") }
+                    appendLine("Hp: " + stats["hp"])
+                    appendLine("Hp per level: " + stats["hpperlevel"])
+                    appendLine("Mp: "+ stats["mp"])
+                    appendLine("Mp per level: "+stats["mpperlevel"])
+                    appendLine("Moves peed: " + stats["movespeed"])
+                    appendLine("Armor: " + stats["armor"])
+                    appendLine("Armor per level: "+ stats["armorperlevel"])
+                    appendLine("Spell block: "+stats["spellblock"])
+                    appendLine("Spell block per level: " + stats["spellblockperlevel"])
+                    appendLine("Attack range: " + stats["attackrange"])
+                    appendLine("Attack Speed: "+ stats["attackspeed"])
+
+                }
+            }
+        }
     }
 }
