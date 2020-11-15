866987b1b4d48e696264066396a507ba56a21e41
jjlii <jjtommyliii@gmail.com>
2020-11-15 12:20:19 +0100

    List champion (#1)
    * Created main activity
    
    * Created coroutineScopeActivity
    
    * Add viewbinding
    
    * WIP adapter

---
+++ app/src/main/java/com/example/leagueoflegendschampions/ChampionAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.leagueoflegendschampions
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ChampionAdapter {
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    }
+}
