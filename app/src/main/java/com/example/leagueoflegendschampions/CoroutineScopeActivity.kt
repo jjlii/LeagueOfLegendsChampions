ccc2842e15f5a8e73ac4c3636ad9acb49b5a7fca
Junjie Li <jjtommyliii@gmail.com>
2020-11-15 12:03:43 +0100

    Created coroutineScopeActivity

---
+++ app/src/main/java/com/example/leagueoflegendschampions/CoroutineScopeActivity.kt
@@ -0,0 +1,28 @@
+package com.example.leagueoflegendschampions
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlin.coroutines.CoroutineContext
+
+abstract class CoroutineScopeActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        job = SupervisorJob()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+}
