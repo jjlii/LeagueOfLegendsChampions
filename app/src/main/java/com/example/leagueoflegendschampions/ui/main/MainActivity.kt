5b8e88bb2ab6f3bdc3a085a3eca7c1cb814a5dfb
Junjie Li <jjtommyliii@gmail.com>
2020-11-28 18:18:25 +0100

    Refactor MainActivity to implement mvvm

--- app/src/main/java/com/example/leagueoflegendschampions/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/leagueoflegendschampions/ui/main/MainActivity.kt
@@ -1,50 +1,50 @@
 package com.example.leagueoflegendschampions.ui.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.example.leagueoflegendschampions.databinding.ActivityMainBinding
 import com.example.leagueoflegendschampions.module.Champion
 import com.example.leagueoflegendschampions.module.ChampionRepository
 import com.example.leagueoflegendschampions.ui.detail.DetailActivity
 import com.example.leagueoflegendschampions.ui.commun.startActivity
+import com.example.leagueoflegendschampions.ui.main.MainViewModel.UiModel
+import com.example.leagueoflegendschampions.ui.main.MainViewModel.UiModel.*
 
-class MainActivity : AppCompatActivity(), MainPresenter.View {
+class MainActivity : AppCompatActivity(){
 
-    private  val presenter by lazy {  MainPresenter(ChampionRepository(this))}
+    private lateinit var viewModel: MainViewModel
     private lateinit var binding : ActivityMainBinding
+    private lateinit var adapter : ChampionAdapter
 
-    private val adapter = ChampionAdapter {
-        presenter.onChampionClick(it)
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        presenter.onCreate(this)
+        viewModel = ViewModelProvider(this,
+                MainViewModelFactory(ChampionRepository(this)))
+                .get()
+
+        adapter =ChampionAdapter(viewModel::onChampionClick)
         binding.championListView.adapter = adapter
+
+        viewModel.model.observe(this, Observer(::updateUi))
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        presenter.onDestroy()
-    }
-
-    override fun showProgress() {
-        binding.progress.visibility = android.view.View.VISIBLE
-    }
-
-    override fun hideProgress() {
-        binding.progress.visibility = android.view.View.GONE
-    }
-
-    override fun updateData(champions: List<Champion>) {
-        adapter.championList = champions
-    }
-
-    override fun navigateTo(champion: Champion) {
-        startActivity<DetailActivity> {
-            putExtra(DetailActivity.CHAMPION, champion)
+    private fun updateUi( model: UiModel){
+        if (model !is Loading){
+            binding.progress.visibility = View.GONE
+        }
+        when(model){
+            is Content -> adapter.championList = model.champions
+            is Navigation -> startActivity<DetailActivity>{
+                putExtra(DetailActivity.CHAMPION, model.champion)
+            }
+            is Loading -> binding.progress.visibility = View.VISIBLE
         }
     }
 }
---
+++ app/src/main/java/com/example/leagueoflegendschampions/ui/main/MainViewModel.kt
@@ -0,0 +1,57 @@
+package com.example.leagueoflegendschampions.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.leagueoflegendschampions.module.Champion
+import com.example.leagueoflegendschampions.module.ChampionRepository
+import com.example.leagueoflegendschampions.ui.commun.Scope
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val championRepository: ChampionRepository):
+    ViewModel(),Scope by Scope.Impl() {
+
+    sealed class UiModel{
+        object Loading : UiModel()
+        class Content(val champions : List<Champion>) : UiModel()
+        class Navigation(val champion: Champion) : UiModel()
+    }
+
+    private val _model = MutableLiveData<UiModel>()
+    val model: LiveData<UiModel>
+        get(){
+            if (_model.value ==null)
+                refresh()
+            return _model
+        }
+
+    init {
+        initScope()
+    }
+
+    private fun refresh() {
+        launch {
+            _model.value = UiModel.Loading
+            val championList = championRepository.getChampions()
+            _model.value = UiModel.Content(championList.data.values.toList())
+        }
+    }
+
+    override fun onCleared() {
+        cancelScope()
+        super.onCleared()
+    }
+
+    fun onChampionClick(champion: Champion) {
+        _model.value = UiModel.Navigation(champion)
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+class MainViewModelFactory(private val championRepository: ChampionRepository):
+    ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+        MainViewModel(championRepository) as T
+
+}
