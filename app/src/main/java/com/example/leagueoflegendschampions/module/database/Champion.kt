2f4ecc5a30aedeeb9d9481835c374f6500e8a614
Junjie Li <jjtommyliii@gmail.com>
2020-12-14 20:33:36 +0100

    Add converter for entitie

--- app/src/main/java/com/example/leagueoflegendschampions/module/database/Champion.kt
+++ app/src/main/java/com/example/leagueoflegendschampions/module/database/Champion.kt
@@ -2,11 +2,12 @@ package com.example.leagueoflegendschampions.module.database
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
 
 @Entity
 data class Champion(
         @PrimaryKey
-        val id: Int,
+        val id: String,
         val name: String,
         val square: String,
         val blurb: String,
@@ -16,7 +17,9 @@ data class Champion(
         val defense: Long,
         val magic: Long,
         val difficulty: Long,
+        @TypeConverters(TagsConverter::class)
         val tags: List<String>,
+        @TypeConverters(StatsConverter::class)
         val stats: Map<String, Double>,
         val favorite: Boolean
 )
--- app/src/main/java/com/example/leagueoflegendschampions/module/database/ChampionDatabase.kt
+++ app/src/main/java/com/example/leagueoflegendschampions/module/database/ChampionDatabase.kt
@@ -2,9 +2,11 @@ package com.example.leagueoflegendschampions.module.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [Champion::class], version = 1)
+@TypeConverters(StatsConverter::class, TagsConverter::class)
 abstract class ChampionDatabase : RoomDatabase() {
 
-    abstract fun championDao(): Champion
+    abstract fun championDao(): ChampionDao
 }
