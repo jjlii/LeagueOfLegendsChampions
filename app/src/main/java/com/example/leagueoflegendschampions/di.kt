d012dc6249c879eaaa10a84674bc7ed85eaa8b08
jjlii <jjtommyliii@gmail.com>
2021-01-30 16:17:22 +0100

    Koin implemented (#12)

---
+++ app/src/main/java/com/example/leagueoflegendschampions/di.kt
@@ -0,0 +1,70 @@
+package com.example.leagueoflegendschampions
+
+import android.app.Application
+import com.example.data.PermissionChecker
+import com.example.data.repository.ChampionRepository
+import com.example.data.repository.RegionRepository
+import com.example.data.source.LocalDataSource
+import com.example.data.source.LocationDataSource
+import com.example.data.source.RemoteDataSource
+import com.example.leagueoflegendschampions.data.AndroidPermissionChecker
+import com.example.leagueoflegendschampions.data.PlayServicesLocationDataSource
+import com.example.leagueoflegendschampions.data.database.ChampionDatabase
+import com.example.leagueoflegendschampions.data.database.RoomDataSource
+import com.example.leagueoflegendschampions.data.server.ChampionDbDataSource
+import com.example.leagueoflegendschampions.ui.detail.DetailViewModel
+import com.example.leagueoflegendschampions.ui.detail.DetailsFragment
+import com.example.leagueoflegendschampions.ui.main.MainFragment
+import com.example.leagueoflegendschampions.ui.main.MainViewModel
+import com.example.usecases.FindChampionByIdUseCase
+import com.example.usecases.GetChampionsUseCase
+import com.example.usecases.ToggleChampionFavoriteUseCase
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+
+fun Application.initDI(){
+    startKoin {
+        androidLogger()
+        androidContext(this@initDI)
+        modules(listOf(appModule, dataModule, scopeModule))
+        //modules(listOf(appModule, dataModule))
+    }
+}
+
+private val dataModule = module {
+    factory { ChampionRepository(get(), get(), get()) }
+    factory { RegionRepository(get(), get()) }
+}
+
+private val appModule = module {
+    single { ChampionDatabase.build(get()) }
+    factory <LocalDataSource>{ RoomDataSource(get()) }
+    factory <RemoteDataSource>{ ChampionDbDataSource() }
+    factory <LocationDataSource>{ PlayServicesLocationDataSource(get()) }
+    factory <PermissionChecker>{ AndroidPermissionChecker(get()) }
+
+    /*
+    viewModel { MainViewModel(get()) }
+    factory { GetChampionsUseCase(get()) }
+    viewModel { (id: String ) ->DetailViewModel(id, get(), get()) }
+    factory { FindChampionByIdUseCase(get()) }
+    factory { ToggleChampionFavoriteUseCase(get()) }
+     */
+}
+
+private val scopeModule = module {
+    scope(named<MainFragment>()){
+        viewModel { MainViewModel(get()) }
+        scoped { GetChampionsUseCase(get()) }
+    }
+
+    scope(named<DetailsFragment>()){
+        viewModel { (id: String ) ->DetailViewModel(id, get(), get()) }
+        scoped { FindChampionByIdUseCase(get()) }
+        scoped { ToggleChampionFavoriteUseCase(get()) }
+    }
+}
